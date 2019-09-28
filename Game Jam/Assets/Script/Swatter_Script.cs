22d53531b03a4a0351e86279aa85fc785c3fa333
Jonathan Koch <47391518+JonathanSKoch@users.noreply.github.com>
2019-09-28 18:14:58 -0400

    Added test scene

---
+++ Game Jam/Assets/Script/Swatter_Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swatter_Script : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            this.transform.position += new Vector3(-1, 0, 0);
+    }
+}
